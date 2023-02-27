c735915ccc1fc315e817ab814f448e76ff86c0df
Murilo Martins <murilomartins@outlook.com>
2023-02-27 11:36:09 -0300

    :construction: NotFound route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import HomeIndex from "./routes/Home/HomeIndex";
+import NotFound from "./routes/NotFound";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Home />}>
             <Route index element={<HomeIndex />} />
         </Route>
+        <Route path={"*"} element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routes/NotFound/index.tsx
@@ -0,0 +1,12 @@
+import MainHeader from "../../components/MainHeader";
+
+export default function NotFound() {
+  return (
+    <>
+      <MainHeader />
+      <main>
+        <h1 className="rr-container rr-mt20 rr-page-title">Não encontrado!</h1>
+      </main>
+    </>
+  );
+}
